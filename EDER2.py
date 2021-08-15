541132a037cb3be9af16c265c7ad2907c1f5c553
Milcomeda <86146232+Milcomeda@users.noreply.github.com>
2021-08-16 00:54:39 +0900

    clik成功

--- EDER2.py
+++ EDER2.py
@@ -2,15 +2,20 @@ import subprocess
 import webbrowser
 import eel
 
-def main():
+
+def Open():
     eel.init('web', allowed_extensions=['.js', '.html', '.css'])
     eel.start('html\EDER2.html')
 
+
 @eel.expose
-def link_click(name):
-    # print('ok')
-    # subprocess.Popen(["explorer", r"C:\Users\WAVE\Desktop\TRY"], shell=True)
+def Folder_Open(name):
     subprocess.Popen(["explorer", name], shell=True)
 
+@eel.expose
+def File_Open(name):
+    subprocess.Popen(["start", name], shell=True)
+
+
 if __name__ == '__main__':
-    main()
+    Open()
