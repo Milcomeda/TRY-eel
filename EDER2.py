91d0f06874de08941da06f12bee28396366b1083
Milcomeda <86146232+Milcomeda@users.noreply.github.com>
2021-08-25 21:51:10 +0900

    8/25

--- EDER2.py
+++ EDER2.py
@@ -5,16 +5,16 @@ import eel
 
 def Open():
     eel.init('web', allowed_extensions=['.js', '.html', '.css'])
-    eel.start('html\EDER2.html')
+    eel.start('html\EDER2.html', mode='edge')
 
 
 @eel.expose
 def Folder_Open(name):
-    subprocess.Popen(["explorer", name], shell=True)
+    subprocess.Popen(["explorer", "\\" + name], shell=True)
 
 @eel.expose
 def File_Open(name):
-    subprocess.Popen(["start", name], shell=True)
+    subprocess.Popen(["start", "\\" + name], shell=True)
 
 
 if __name__ == '__main__':
